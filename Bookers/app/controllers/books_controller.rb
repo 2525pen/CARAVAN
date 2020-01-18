50c1f983561ae01930d9472fad22b1aced04ec9c
“2525pen” <sakai3ons@gmail.com>
2020-01-18 21:07:04 +0900

    [finish]4-8

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,41 +1,57 @@
 class BooksController < ApplicationController
-  def index
-  	@book = Book.all
-  end
+    def index
+    	@books = Book.all
+      @book = Book.new
+    end
 
-  def show
-    @book = Book.find(params[:id])
-  end
+    def top
+    end
 
-  def new
-    @book = Book.new
-  end
+    def show
+      @book = Book.find(params[:id])
+    end
 
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path
-  end
+    def new
+      @book = Book.new
+    end
 
-  def edit
-    @book = Blog.find(params[:id])
-  end
+    def create
+      @book = Book.new(book_params)
+      if
+        @book.save
+        flash[:notice] = "Book was successfully created."
+        redirect_to book_path(@book)
+      else
+        @books = Book.all
+        render 'index'
+      end
+    end
 
-  def update
-    book =Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
-  end
+    def edit
+      @book = Book.find(params[:id])
+    end
 
-  def destoroy
-    book = Book.find(params[:id])
-    book.destoroy
-    redirect_to book_path
-  end
+    def update
+      @book = Book.find(params[:id])
+      if
+        @book.update(book_params)
+        flash[:notice] = "Book was successfully created."
+        redirect_to book_path(@book)
+      else
+        # @book = Book.find(params[:id])
+        render 'edit'
+      end
+    end
 
-  private
-	def book_params
-	  params.require(:blog).permit(:title, :body)
+    def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
+    end
+
+    private
+      def book_params
+      params.require(:book).permit(:title, :body)
     end
 end
 
--- Bookers/app/models/book.rb
+++ Bookers/app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+	# 空の投稿を防ぐバリデーション
+	validates :title, presence:true
+	validates :body, presence:true
 end
